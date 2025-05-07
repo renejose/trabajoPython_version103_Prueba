746e0b8bc7f22a5f61fd13bc1b7c94cd2be267f9
RONALD STITH LÓPEZ <rostilo@gmail.com>
2025-05-06 21:52:25 -0500

    nuevo script

---
+++ vervideos.py
@@ -0,0 +1,8 @@
+import webbrowser
+
+# URL del video de YouTube que deseas ver
+video_url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'  # Reemplaza con el enlace del video que desees
+
+# Abre el video en el navegador predeterminado
+webbrowser.open(video_url)
+
