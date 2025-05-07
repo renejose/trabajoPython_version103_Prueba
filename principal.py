8b6b052fda48d6a071096ad6973dd79527b9fdc3
Marco <marco.amesquita@gmail.com>
2025-05-06 21:34:30 -0500

    pruebas MarcoAmesquita

--- principal.py
+++ principal.py
@@ -1,1 +1,16 @@
 print("============== bienvenidos al proyecto================")
+
+def es_primo(n):
+    """Determina si un número es primo."""
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+# Ejemplo de uso
+numero = 29
+if es_primo(numero):
+    print(f"{numero} es un número primo.")
+else:
+    print(f"{numero} no es un número primo.")
